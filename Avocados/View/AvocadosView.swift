e25fb91ab6ddaed4b2120c9df3179c90a5a32b0c
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-03-01 18:06:26 +0200

    rewritten the onAppear .animation withAnimation since . animation was depreacted accoridnig to xCode hint at iOS 15

--- Avocados/View/AvocadosView.swift
+++ Avocados/View/AvocadosView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    @State private var pulsateAnimation: Bool = false
     var body: some View {
         VStack {
             Spacer()
@@ -16,8 +17,14 @@ struct AvocadosView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 240, height: 240, alignment: .center)
                 .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+                .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+                .opacity(self.pulsateAnimation ? 1 : 0.9)
+                
+                          
+                          
+            
             VStack {
-                Text("Avocados")
+                Text("Avocados".uppercased())
                     .font(.system(size: 42, weight: .bold, design: .serif))
                     .foregroundStyle(Color.white)
                     .padding()
@@ -43,6 +50,14 @@ Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado r
                 .aspectRatio(contentMode: .fill)
             )
         .ignoresSafeArea(.all)
+        .onAppear {
+            withAnimation(
+                Animation.easeInOut(duration: 1.5)
+                    .repeatForever(autoreverses: true)
+            ){
+                self.pulsateAnimation.toggle()
+            }
+        }
     }
 }
 
